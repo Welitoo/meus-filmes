4982f11fb3e90a5572a66843c0249dd8feb9cad0
aluno <aluno@LAB.ETEC187>
2022-09-05 22:24:10 -0300

    Finalizando interface da tela Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
 
 export default function Home(){
      return(
-        <View>
-            <text>Meus Filmes</text>
-            <text>Olá Welito, boa noite!</text>
+        <View style={styles.container}>
+            <Text style={styles.título}>Meus Filmes</Text>
+            <Text style={styles.Subtítulo}>Olá Welito, boa noite!</Text>
 
-            <TouchableOpacity>
-                <Text>Adicionar</Text>
+            <TextInput style={styles.campo} placeholder="Nome do Filme"></TextInput>
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Adicionar</Text>
             </TouchableOpacity>
         </View>
      );
@@ -17,5 +19,46 @@ export default function Home(){
 
 //Criando os estilos
 const styles = StyleSheet.create({
- 
-});
+    container:{
+        flex: 1,
+        backgroundColor: '#30478C',
+        paddingVertical: 70,
+        paddingHorizontal: 20,
+    },
+        título:{
+            fontSize: 50,
+            fontWeight: 'bold',
+            marginBottom: 10,
+            alignSelf: 'center'  
+    },
+
+    Subtítulo:{
+        color: '#FFF',
+        fontSize: 30
+    },
+    campo:{
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        marginTop: 30,
+        borderRadius: 7,
+        padding: 15
+    },
+
+    greetings: {
+        color: '#FFF'
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold'
+    },
+
+}); //Fim dos estilos
